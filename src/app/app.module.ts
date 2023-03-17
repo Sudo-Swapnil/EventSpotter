95108a75366a46870112732c6f67fd45faeffdd4
Swapnil Chhatre <code.schhatre@gmail.com>
2023-03-17 11:55:34 -0700

    Add API call to backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { FavouritesComponent } from './favourites/favourites.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { FavouritesComponent } from './favourites/favourites.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  formValues = {
+    keyword: ''
+  }
 
+  constructor (private http: HttpClient) {}
+
+  submitForm(form: NgForm){
+    this.formValues.keyword = form.value.keyword;
+    console.log("name value: ", this.formValues.keyword)
+    this.makeGetRequest();
+  }
+
+  makeGetRequest(){
+    // let reqParams = new HttpParams().set('name', this.formValues.keyword);
+    // console.log("From makeGetRequest function: ", reqParams)
+    // let url = "localhost:3000/api/tkm" + reqParams.toString()
+    
+    // console.log(reqParams)
+    // this.http.get('localhost:3000/api/tkm', { reqParams }).subscribe((data) => {
+    //   console.log(data);
+    // })
+    console.log("Making request...")
+    let params = new HttpParams();
+    params = params.append('keyword', this.formValues.keyword)
+    console.log('http://localhost:3000/api/tkm', {params})
+    let result = this.http.get('http://localhost:3000/api/tkm', {params})
+    console.log(result.subscribe((data) => {
+      console.log(data)
+    }))
+    console.log("Request complete...")
+
+  }
 }
